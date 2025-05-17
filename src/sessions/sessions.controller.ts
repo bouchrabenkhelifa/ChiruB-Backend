06944098cdba841f3a735b8435e1505483ef05d2
bouchrabenkhelifa <benbouchra317@gmail.com>
2025-05-17 11:04:47 -0300

    admin done

--- src/sessions/sessions.controller.ts
+++ src/sessions/sessions.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Query, Param } from '@nestjs/common'; 
+import { Controller, Get, Query, Param ,Body,Post} from '@nestjs/common'; 
 import { SessionsService } from './sessions.service';
+import { CreateSessionDto } from './create-session.dto';
 @Controller('sessions')
 export class SessionsController {
       constructor(private readonly sessionsService: SessionsService) {}
@@ -9,5 +10,14 @@ export class SessionsController {
       const users = await this.sessionsService.getSessions();
       return users;
     }
-    
+  @Post()
+  async create(@Body() dto: CreateSessionDto) {
+    return this.sessionsService.create(dto);
+  }
+
+  @Get('hos')
+async getHospitals() {
+  const hospitals = await this.sessionsService.getHospitals();
+  return hospitals;
+}
 }
